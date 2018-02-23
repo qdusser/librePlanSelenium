1a5168f9e4e71b6eaaa6781f0142a9a23796b891
quentin Dusser <qdusser@qualixo.com>
2018-02-23 16:48:40 +0100

    CHANGEMENT URL ==> GUILLAUME

--- src/test/java/fr/eql/autom/LibrePlan/GRE_02Test.java
+++ src/test/java/fr/eql/autom/LibrePlan/GRE_02Test.java
@@ -77,7 +77,7 @@ public class GRE_02Test {
 		wd = new ChromeDriver();*/
 			
 	//acc�s � l'url
-	wd.get("http://localhost:8082/libreplan");
+	wd.get("http://192.168.2.103:8082/libreplan");
 	wd.manage().window().maximize();
 	wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	LogPage pageConnexion = PageFactory.initElements(wd, LogPage.class);
